e6f88df30955cd0a32af017d0ec0504b53c6467d
Aravind R. Kallam <aravinka@petco.com>
2020-03-12 13:48:17 -0700

    changes to test file

--- variables.py
+++ variables.py
@@ -1,16 +1,6 @@
 #Testrail variables 
 ph_project_id = 38
 
-
-Membership = {
-    'suite_id' : 113,
-    'section_id' : 53666,
-    'proj_name' : 'Petco_Membership',
-    'fpath' : "<provide excel path>",
-    'report_id' : 1,
-    'milestone_id' : 124
-}
-
 Wellness = {
     'suite_id' : 113,
     'section_id' : 56067,
@@ -18,13 +8,4 @@ Wellness = {
     'fpath' : "<provide excel path>",
     'report_id' : 3,
     'milestone_id': 126
-}
-
-InHome = {
-    'suite_id' : 113,
-    'section_id' : 53478,
-    'proj_name' : 'Petco_InHomeTraining',
-    'fpath' : "<provide excel path>",
-    'report_id' : 2,
-    'milestone_id': 125
 }
